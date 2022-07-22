b63c713bd8b3886c7780e1476bef3509ac888aaa
damatechguy27 <104665781+damatechguy27@users.noreply.github.com>
2022-07-22 17:32:04 -0400

    Delete VMs/tf-vm-anis-iis directory

--- VMs/tf-vm-anis-iis/winrm_setup.ps1
+++
@@ -1,4 +0,0 @@
-﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1
-wget "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1" -Outfile ConfigureRemotingForAnsible.ps1 
-./ConfigureRemotingForAnsible.ps1 -EnableCredSSP -DisableBasicAuth -Verbose
